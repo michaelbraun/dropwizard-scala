481156ed1d8fbfd1d6dd176840308aa15a9a96ab
Cliff James <clifford.h.james@gmail.com>
2015-02-22 02:07:30 -0500

    Add ScalaValidationSupport from datasift

--- src/main/scala/com/massrelevance/dropwizard/bundles/ScalaBundle.scala
+++ src/main/scala/com/massrelevance/dropwizard/bundles/ScalaBundle.scala
@@ -1,16 +1,21 @@
 package com.massrelevance.dropwizard.bundles
 
+import com.massrelevance.dropwizard.scala.validation.ScalaValidatorsBundle
 import io.dropwizard.Bundle
 import io.dropwizard.setup.{Bootstrap, Environment}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.massrelevance.dropwizard.scala.inject.ScalaCollectionsQueryParamInjectableProvider
 
 class ScalaBundle extends Bundle {
+  val validatorsBundle = new ScalaValidatorsBundle
+
   def initialize(bootstrap: Bootstrap[_]) {
     bootstrap.getObjectMapper.registerModule(new DefaultScalaModule())
+    validatorsBundle.initialize(bootstrap)
   }
 
   def run(environment: Environment) {
     environment.jersey.getResourceConfig.getClasses.add(classOf[ScalaCollectionsQueryParamInjectableProvider])
+    validatorsBundle.run(environment)
   }
 }
